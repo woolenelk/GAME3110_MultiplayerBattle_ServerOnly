6196f1aae3d9b3e538f30aedd0b3c57bef826dcd
Michael Denkovski <michaeldenkovski@hotmail.com>
2020-10-10 16:58:44 -0400

    Added in player UI elements

---
+++ Assets/_Scripts/BackToLoginBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToLoginBehaviour : MonoBehaviour
+{
+    public void OnBackToLoginButtonPressed()
+    {
+        Debug.Log("Back To Login Button Pressed");
+        SceneManager.LoadScene("Login");
+    }
+}
