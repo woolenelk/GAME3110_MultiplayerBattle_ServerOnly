2af8dd654db144fdaa601fbd8873b2829076c8a5
Michael Denkovski <michaeldenkovski@hotmail.com>
2020-10-10 16:19:24 -0400

    Created Start, Login, and play scenes with some transitions wetween
    added button transitions for start scene to lin scene and from login scene to play scene

---
+++ Assets/_Scripts/LoginButtonBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginButtonBehaviour : MonoBehaviour
+{
+    public void OnLoginButtonPressed()
+    {
+        Debug.Log("Login Button Pressed");
+        SceneManager.LoadScene("Play");
+    }
+}
---
+++ Assets/_Scripts/PlayButtonBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButtonBehaviour : MonoBehaviour
+{
+   public void OnPlayButtonPressed()
+    {
+        Debug.Log("Play Button Pressed");
+        SceneManager.LoadScene("Login");
+    }
+}
