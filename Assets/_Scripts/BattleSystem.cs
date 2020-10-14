461ccde6ec226bd608cdbe1bf9c901382c13b216
Michael Denkovski <michaeldenkovski@hotmail.com>
2020-10-14 16:16:05 -0400

    setup transition into first players turn

--- Assets/_Scripts/BattleSystem.cs
+++ Assets/_Scripts/BattleSystem.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -35,10 +36,11 @@ public class BattleSystem : MonoBehaviour
     void Start()
     {
         state = BattleState.START;
-        CreatePlayers();
+        StartCoroutine(CreatePlayers());
     }
 
-    void CreatePlayers()
+    //a corutine to allow for us to have delays
+    IEnumerator CreatePlayers()
     {
         //instantiate player and get their detials
         GameObject player = Instantiate(playerCharacter);
@@ -54,6 +56,19 @@ public class BattleSystem : MonoBehaviour
         playerDetailsUI.FillUI(playerDetails);
         enemyDetailsUI.FillUI(enemyDetails);
 
+        yield return new WaitForSeconds(3.0f);
+
+        //currently have player 1 always start
+        state = BattleState.PLAYER1;
+
+        Player1Turn();
+
+    }
+
+
+    void Player1Turn()
+    {
+        dialogueText.text = "What would you like your waifu to do?";
     }
 
 
