562f6fcbc1139a955c7e1ea1f09e2ccee65cd162
Michael Denkovski <michaeldenkovski@hotmail.com>
2020-10-14 16:08:26 -0400

    update UI stats per enemy and player character

--- Assets/_Scripts/BattleSystem.cs
+++ Assets/_Scripts/BattleSystem.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.XR.WSA.Input;
 
 public enum BattleState { 
     START,
@@ -14,9 +14,22 @@ public enum BattleState {
 
 public class BattleSystem : MonoBehaviour
 {
+    //knows the state of the current game
     public BattleState state;
+    //which characters to instantiate
     public GameObject playerCharacter;
     public GameObject enemyCharacter;
+
+    //details of our charatcer
+    WaifuDetails playerDetails;
+    WaifuDetails enemyDetails;
+
+    public TextMeshProUGUI dialogueText;
+
+    // the UI panels for the respective characters
+    public DetailsUI playerDetailsUI;
+    public DetailsUI enemyDetailsUI;
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,8 +40,20 @@ public class BattleSystem : MonoBehaviour
 
     void CreatePlayers()
     {
-        Instantiate(playerCharacter);
-        Instantiate(enemyCharacter);
+        //instantiate player and get their detials
+        GameObject player = Instantiate(playerCharacter);
+        playerDetails = player.GetComponent<WaifuDetails>();
+
+        //instantiate enemy and get their detials
+        GameObject enemy = Instantiate(enemyCharacter);
+        enemyDetails = enemy.GetComponent<WaifuDetails>();
+
+
+        dialogueText.text = "You face off against " + enemyDetails.characterName;
+
+        playerDetailsUI.FillUI(playerDetails);
+        enemyDetailsUI.FillUI(enemyDetails);
+
     }
 
 
---
+++ Assets/_Scripts/DetailsUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetailsUI : MonoBehaviour
+{
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI healthText;
+    public Slider slider;
+
+
+    public void FillUI(WaifuDetails details)
+    {
+        nameText.text = details.characterName;
+
+        string text = "" + details.health.ToString() + "/" + details.healthMax.ToString();
+        healthText.SetText(text, true);
+
+        //Debug.Log(slider.value);
+        slider.maxValue = details.healthMax;
+        slider.value = details.health;
+
+    }
+
+    public void UpdateHP(float health)
+    {
+        slider.value = health;
+        healthText.text = "" + health.ToString() + "/" + slider.maxValue.ToString();
+    }
+
+
+}
