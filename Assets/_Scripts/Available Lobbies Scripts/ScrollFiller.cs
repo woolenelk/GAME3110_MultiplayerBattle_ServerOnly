ecade32d2de764fc1b02cef5a4157fb52ef26b04
Michael Denkovski <michaeldenkovski@hotmail.com>
2020-12-12 15:43:09 -0500

    available lobbies basic functionality

---
+++ Assets/_Scripts/Available Lobbies Scripts/ScrollFiller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScrollFiller : MonoBehaviour
+{
+    public ScrollRect scrollView;
+    public GameObject scrollContent;
+    public GameObject scrollItemprefab;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            GenerateItem(i);
+        }
+        scrollView.verticalNormalizedPosition = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateItem(int level)
+    {
+        GameObject scrollItemObj = Instantiate(scrollItemprefab);
+        scrollItemObj.transform.SetParent(scrollContent.transform, false);
+        scrollItemObj.transform.Find("PlayerLevel").gameObject.GetComponent<TMP_Text>().text = level.ToString();
+    }
+}
