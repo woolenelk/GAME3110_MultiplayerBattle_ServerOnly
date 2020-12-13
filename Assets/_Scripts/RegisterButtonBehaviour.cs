7e3c6995009e3953047f58c5cec7316a95c96057
Michael Denkovski <michaeldenkovski@hotmail.com>
2020-12-12 19:07:15 -0500

    added register button behaviour

---
+++ Assets/_Scripts/RegisterButtonBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+
+public class RegisterButtonBehaviour : MonoBehaviour
+{
+    public GameObject errorMessage;
+    public float errorMessageDuration;
+    IEnumerator errorMessageCoroutine;
+
+    [SerializeField]
+    TMP_InputField usernameInput;
+
+    [SerializeField]
+    TMP_InputField passwordInput;
+
+    private void Start()
+    {
+        errorMessage.SetActive(false);
+    }
+
+    public void OnRegisterButtonPressed()
+    {
+
+        NetworkClient networkManager = FindObjectOfType<NetworkClient>();
+        networkManager.Register(usernameInput.text, passwordInput.text);
+
+
+        //if credentials valid move to lobbies scene
+        Debug.Log("Register Button Pressed");
+        //SceneManager.LoadScene("Lobbies");
+
+        //if credentials fail display error notification of incorrect credentials
+        if (errorMessageCoroutine != null)
+        {
+            StopCoroutine(errorMessageCoroutine);
+
+        }
+        errorMessageCoroutine = DisplayErrorMessage();
+        StartCoroutine(errorMessageCoroutine);
+    }
+
+
+    IEnumerator DisplayErrorMessage()
+    {
+        errorMessage.SetActive(true);
+        yield return new WaitForSeconds(errorMessageDuration);
+        errorMessage.SetActive(false);
+    }
+}
